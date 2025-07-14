ce946a498d1074c852399432ced0e613b7ba8aae
Fernando Warno <ferwar@hotmail.com>
2025-07-14 17:35:35 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MyButton from "./components/MyButton";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola React</h1>
+      <MyButton />
+    </>
+  );
+};
+export default App;
---
+++ src/components/MyButton.tsx
@@ -0,0 +1,6 @@
+import "./MyButton.css";
+
+const MyButton = () => {
+  return <button className="btn">MyButton</button>;
+};
+export default MyButton;
